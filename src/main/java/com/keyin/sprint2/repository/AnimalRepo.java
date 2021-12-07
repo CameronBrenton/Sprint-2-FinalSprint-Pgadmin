b78eb65efdb34501b3725c5ec8a8b261b7e36bd0
jacobsheppard <jacob.sheppard@keyin.com>
2021-12-07 10:46:39 -0330

    added a animal repository, added maven dependancys and imports and created the public class interface

---
+++ src/main/java/com/keyin/sprint2/repository/AnimalRepo.java
@@ -0,0 +1,16 @@
+package com.keyin.sprint2.repository;
+
+import com.keyin.sprint2.model.Animal;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "animal", path = "animal")
+public interface AnimalRepo extends PagingAndSortingRepository<Animal, Long> {{
+
+        List<Animal> findByLastName(@Param("animalName") String animalName);
+
+    }
+}

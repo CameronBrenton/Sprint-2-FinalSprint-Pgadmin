5dc08602ec1be77fe7b1fe1f31cc4be2a5e65cbe
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-13 15:24:44 -0330

    First commit of User model

---
+++ src/main/java/com/keyin/sprint2/model/User.java
@@ -0,0 +1,35 @@
+package com.keyin.sprint2.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String email;
+    private String password;
+
+
+    public User(){
+
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+

423d0cfd34bcd74de1a5626c06cd5240bb3bd9a3
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-14 09:46:04 -0330

    change PagingAndSortingRepository to CrudRepository, doing some testing

--- src/main/java/com/keyin/sprint2/repository/AnimalRepo.java
+++ src/main/java/com/keyin/sprint2/repository/AnimalRepo.java
@@ -1,6 +1,7 @@
 package com.keyin.sprint2.repository;
 
 import com.keyin.sprint2.model.Animal;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,10 +9,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "animal", path = "animal")
-public interface AnimalRepo extends PagingAndSortingRepository<Animal, Long> {
+public interface AnimalRepo extends CrudRepository<Animal, Long> {
 
         List<Animal> findByAnimalName(@Param("animalName") String animalName);
-
+        Iterable<Animal> findAll();
 //        List<Animal> findByAnimal_id(@Param("animal_id") String animal_id);
 //        List<Animal> findBySpecies(@Param("species") String species);
 //        List<Animal> findByAnimalColor(@Param("animalColor") String animalColor);

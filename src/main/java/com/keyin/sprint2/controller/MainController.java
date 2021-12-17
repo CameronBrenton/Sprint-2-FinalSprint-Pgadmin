0c3f101a254f615644dcf5a8ce1d13077a79d19f
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-17 09:06:52 -0330

    Add necessary code for SpringSecurity authentication system. Set up BCrypt

--- src/main/java/com/keyin/sprint2/controller/MainController.java
+++ src/main/java/com/keyin/sprint2/controller/MainController.java
@@ -4,8 +4,10 @@ import com.keyin.sprint2.model.Animal;
 import com.keyin.sprint2.model.Search;
 import com.keyin.sprint2.model.User;
 import com.keyin.sprint2.repository.AnimalRepo;
+import com.keyin.sprint2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -21,14 +23,16 @@ import java.util.Optional;
 @Controller
 public class MainController {
     @Autowired
-    private AnimalRepo animalRepository;
+    private AnimalRepo animalRepo;
+    @Autowired
+    private UserRepository userRepo;
 
-    // Get Mappings
-    @GetMapping(path = "/signin")
-    public String getLogInPage() {
-
-        return "signout";
-    }
+//    // Get Mappings
+//    @GetMapping(path = "/signin")
+//    public String getLogInPage() {
+//
+//        return "signin";
+//    }
 
     @GetMapping(path = "/signout")
     public String getLogOutPage() {
@@ -49,22 +53,11 @@ public class MainController {
     @PostMapping(path = "/search")
     public String submitSearchFormPgAdmin(@ModelAttribute("search")Search search, Model model) {
         List<Animal> listAnimals = new ArrayList<Animal>();
-        animalRepository.findByAnimalName(search.getTopic()).forEach(listAnimals::add);
+        animalRepo.findByAnimalName(search.getTopic()).forEach(listAnimals::add);
         model.addAttribute("listAnimals", listAnimals);
         System.out.println("#######" + listAnimals);
         return "search_results";
     }
-
-//    // Search MongoDB
-//    @GetMapping(path = "/searchMongoDB")
-//    public ModelAndView redirectToSearchFormMongo(ModelMap model) {
-//        Search search = new Search();
-//        model.addAttribute("search", search);
-//        model.addAttribute("attribute", "redirectToSearchFormMongo");
-//        //List<String> databaseList = Arrays.asList("PgAdmin(SQL)", "MongoDB(NoSQL)", "Both");
-//        //model.addAttribute("databaseList", databaseList);
-//        return new ModelAndView("redirect:/localhost:9090/search", model);
-//    }
 
 
     // Signup
@@ -77,8 +70,21 @@ public class MainController {
 
     @PostMapping(path = "/signup")
     public String submitSignupForm(@ModelAttribute("user")User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userRepo.save(user);
         System.out.println(user);
         return "signup_success";
+    }
+
+    // Users
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
     }
 
 }

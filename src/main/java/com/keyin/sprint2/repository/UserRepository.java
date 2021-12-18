a469cca803d94750db99c4b5a00d17b90a13cd02
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-18 10:38:02 -0330

    Implement logging of keywords into database

--- src/main/java/com/keyin/sprint2/repository/UserRepository.java
+++ src/main/java/com/keyin/sprint2/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.keyin.sprint2.repository;
 import com.keyin.sprint2.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findByEmail(String email);
